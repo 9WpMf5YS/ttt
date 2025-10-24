b4e5ebfd0211e3c5222f3dc12c5082fd9913af55
mexxi72 <mexxi72@gmail.com>
2025-10-24 13:42:24 +0200

    update to version 1.2.6

---
+++ composeApp.mjs
@@ -0,0 +1,20 @@
+
+import * as Li9za2lrby5tanM from './skiko.mjs';
+import * as _ref_QGpzLWpvZGEvY29yZQ_ from '@js-joda/core';
+import { instantiate } from './composeApp.uninstantiated.mjs';
+import "./custom-formatters.js"
+
+const exports = (await instantiate({
+    './skiko.mjs': Li9za2lrby5tanM,
+    '@js-joda/core': _ref_QGpzLWpvZGEvY29yZQ_
+})).exports;
+
+export const {
+myTimApiLog,
+myTimApiPublishLogRecord,
+timApiReadyCallback,
+memory,
+_initialize
+} = exports
+
+
